4e6b92f10216be4466debed27a82e7205e815505
Minentle Mvunyiswa <102150619+Minentle2301@users.noreply.github.com>
2024-11-23 11:20:58 +0200

    updated error fecting for the api

--- blog-posts/src/App.jsx
+++ blog-posts/src/App.jsx
@@ -8,7 +8,7 @@ const BlogPosts = () => {
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/poss');
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
 
         // Check if the response is not okay
         if (!response.ok) {
